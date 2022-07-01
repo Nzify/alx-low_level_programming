644f7f409ef0783389283c22db9bfa6fc42db112
Nzify <maidazie@gmail.com>
2022-07-01 12:18:52 -0700

    String nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,51 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * _strdup - function that returns a pointer to a newly allocated space
- * in memory, which contains a copy of the string given as a parameter
- *
- * @str: string of chars
- *
- * Return: address of the newly allocated memory
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n: index
+ * Return: char pointer
  */
 
-char *_strdup(char *str)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len;
-	unsigned int i, j;
-	char *str_copy;
-	char *tmp = str;
+char *p;
+unsigned int size1 = 0, size2 = 0, i;
 
-	if (str == NULL)
-		return (NULL);
+if (s1 == NULL)
+s1 = "";
 
-	i = 0;
-	while (*str++)
-		i++;
-	len = i;
-	str = tmp;
+if (s2 == NULL)
+s2 = "";
 
-	str_copy = malloc(len * sizeof(char) + 1);
-	if (str_copy == NULL)
-		return (NULL);
+while (s1[size1] != '\0')
+{
+size1++;
+}
 
-	j = 0;
-	while (j < len)
-	{
-		str_copy[j] = str[j];
-		j++;
-	}
-	str_copy[j] = '\0';
-	return (str_copy);
+while (s2[size2] != '\0')
+{
+size2++;
+}
+
+if (n > size2)
+n = size2;
+p = malloc((size1 + n + 1) * sizeof(char));
+
+if (p == NULL)
+return (0);
+
+for (i = 0; i < size1; i++)
+{
+p[i] = s1[i];
+
+
+for (; i < (size1 + n); i++)
+{
+p[i] = s2[i - size1];
+}
+p[i] = '\0';
+
+return (p);
 }
